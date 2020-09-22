917ee7d02e8e47ecbfd186633354271e2ec2b593
shekhar <shekharpratapsingh26@gmail.com>
2020-09-22 18:31:02 +0530

    check token

--- users/user.router.js
+++ users/user.router.js
@@ -1,12 +1,13 @@
 
 const { createUser,getUsersById,getUsers,updateUser,deleteuser,login } = require('./user.controller');
 const router = require('express').Router();
+const { checkToken}= require('../auth/token_validation')
 
-router.post('/', createUser);
-router.get('/', getUsers);
-router.get('/:id', getUsersById);
-router.patch('/', updateUser);
-router.delete('/', deleteuser);
+router.post('/',checkToken, createUser);
+router.get('/', checkToken,getUsers);
+router.get('/:id',checkToken, getUsersById);
+router.patch('/', checkToken,updateUser);
+router.delete('/',checkToken, deleteuser);
 router.post('/login',login)
 
 
